941230a31ee0bda52906ce1810785ecdd33637a1
Arystan <71083612+theV1rTual@users.noreply.github.com>
2023-04-26 14:22:56 +0600

    1. add new repository and service 2. add validation for the controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,17 +1,38 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
-import { CreateMessageDto } from "./dtos/create-message.dto";
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    this.messagesService = new MessagesService();
+  }
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll();
+  }
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    console.log(id);
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
+
+    return message;
   }
 }
--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -8,7 +8,21 @@ export class MessagesRepository {
     return messages[id];
   }
 
-  async findAll() {}
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
 
-  async create(message: string) {}
+    return messages;
+  }
+
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+
+    const id = Math.floor(Math.random() * 999);
+
+    messages[id] = { id, content };
+
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
 }
